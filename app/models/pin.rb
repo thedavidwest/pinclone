79cac78208f84de7789cc0fd16718a5525c4dc7c
David West <thedavidwest@gmail.com>
2015-08-17 20:16:13 -0500

    Updated heroku config

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,5 +3,4 @@ class Pin < ActiveRecord::Base
   has_attached_file :image, :styles => {:medium => "300x300>", :thumb => "100x100>" }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
-  attr_accessor :image_file_name
 end
