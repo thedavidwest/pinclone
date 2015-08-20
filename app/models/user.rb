9b5c030c272e911c6593909f0fa0648e3beaa003
David West <thedavidwest@gmail.com>
2015-08-19 19:32:37 -0500

    Added voting, more stylesheet changes, and user avatars

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,7 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :name
 
   has_many :pins
+
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "https://s3.amazonaws.com/bw-pinclone/assets/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150819030747) do
+ActiveRecord::Schema.define(version: 20150819172448) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
@@ -43,6 +43,10 @@ ActiveRecord::Schema.define(version: 20150819030747) do
     t.datetime "updated_at",                          null: false
     t.string   "authentication_token"
     t.string   "name"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token"
