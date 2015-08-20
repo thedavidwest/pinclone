65facdf0436b04794ac26489bb52b3c2731f2604
David West <thedavidwest@gmail.com>
2015-08-19 21:46:22 -0500

    changed pin model

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,7 +1,7 @@
 class PinsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_pin, only: [:show, :edit, :update, :destroy, :upvote]
   before_action :current_user, only: [:edit, :update, :destroy]
+  before_action :set_pin, only: [:show, :edit, :update, :destroy, :upvote]
 
   def index
     @pins = Pin.all
