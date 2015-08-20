31f8dc85a861488eeea7c238228de63e4ac0c856
David West <thedavidwest@gmail.com>
2015-08-19 20:55:10 -0500

    updated production.rb for https

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,7 @@ Rails.application.configure do
       :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
+    },
+    :s3_protocol => :https
   }
 end
